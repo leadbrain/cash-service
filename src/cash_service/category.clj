1f76db5188b43cba405ab30fcd6fb69aeaa0f3a4
leadbrain <leadbrain@icloud.com>
2016-06-07 02:07:21 +0900

    #8 카테고리별 금액을 알수 있도록 카테고리 API에 금액 항목 추가

--- src/cash_service/category.clj
+++ src/cash_service/category.clj
@@ -2,24 +2,36 @@
   (:require [cash-service.db-handler :refer :all]))
 
 (defn init []
-  (create-category-table!))
+  (create-category-table!)
+  (set-category<! {:name "none" :type "in" :money 0}))
 
 (defn destroy []
   (drop-category-table!))
 
 (defn getList []
-  (conj (get-category) {:id 0 :name "none" :type "in"}))
+  (get-category))
 
 (defn contain? [id]
   (not-empty (filter #(= id (% :id)) (getList))))
 
 (defn getItem [id]
-  (if (= id 0)
-    {:name "none" :type "in"}
-    (first (get-category-by-id {:id id}))))
+    (first (get-category-by-id {:id id})))
+
+(defn increaseMoney [id money]
+  (let [balance (+ ((getItem id) :money) money)]
+    (update-category-money! {:id id :money balance})))
+
+(defn decreaseMoney [id money]
+  (let [balance (- ((getItem id) :money) money)]
+    (update-category-money! {:id id :money balance})))
+
+(defn deleteProcess [id]
+  (increaseMoney 1 ((getItem id) :money))
+  (delete-category! {:id id}))
 
 (defn delete [id]
-  (delete-category! {:id id}))
+  (if-not (= id 1)
+    (deleteProcess id)))
 
 (defn setItem [item]
   (set-category<! item))
